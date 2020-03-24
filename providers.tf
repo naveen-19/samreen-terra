80ca4fd6eafa0a30557d3f51e207601da67f1911
samreenimran <55229930+samreenimran@users.noreply.github.com>
2020-03-24 16:11:54 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@
 #
 
 provider "aws" {
-  region  = "us-east-1"
+  region  = "ap-south-1"
   version = "~> 2.0"
   #shared_credentials_file = "/root/.aws/credentials"
   profile                 = "default"
