341effc355b8696f375b9d6552a79ba5d6b1d78a
samreenimran <55229930+samreenimran@users.noreply.github.com>
2020-03-25 10:17:33 +0530

    Update eks-worker-nodes.tf

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -37,6 +37,21 @@ resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryR
   role       = aws_iam_role.demo-node.name
 }
 
+resource "aws_launch_configuration" "demo" {
+  associate_public_ip_address = true
+  iam_instance_profile = aws_iam_instance_profile.demo-node.name
+  image_id = data.aws_ami.eks-worker.id
+  instance_type = "t2.medium"
+  key_name = "wfh-sam"
+  name_prefix = "terraform-eks-demo"
+  security_groups = [aws_security_group.demo-node.id]
+  user_data_base64 = base64encode(local.demo-node-userdata)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_eks_node_group" "demo" {
   cluster_name    = aws_eks_cluster.demo.name
   node_group_name = "demo"
