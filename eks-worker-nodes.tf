470fc95d0cce4ac0c251160f9d7107b298340c70
samreenimran <55229930+samreenimran@users.noreply.github.com>
2020-03-27 14:55:33 +0530

    Update eks-worker-nodes.tf

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -58,11 +58,11 @@ resource "aws_eks_node_group" "demo" {
   node_group_name = "demo"
   node_role_arn   = aws_iam_role.demo-node.arn
   subnet_ids      = aws_subnet.demo[*].id
-  //key_name        = "ritu"
+  
   
   scaling_config {
     desired_size = 1
-    max_size     = 1
+    max_size     = 3
     min_size     = 1
   }
 
