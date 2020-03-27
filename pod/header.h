b09bfc35cb9344383def5572dd04cea4a0492258
samreenimran <55229930+samreenimran@users.noreply.github.com>
2020-03-27 20:09:41 +0530

    Create header.h

---
+++ pod/header.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
