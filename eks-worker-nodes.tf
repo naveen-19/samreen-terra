2fa1e3f234c0420acdc8b7fdd991ef5a530d79d0
samreenimran <55229930+samreenimran@users.noreply.github.com>
2020-03-31 10:13:35 +0530

    Update eks-worker-nodes.tf

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -61,8 +61,8 @@ resource "aws_eks_node_group" "demo" {
   
   
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 3
+    max_size     = 3
     min_size     = 1
   }
 
